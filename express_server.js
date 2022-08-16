39f9064fcb122a7685ac327423515f6a595c592e
Matthew Davis <matc.davis@gmail.com>
2022-08-16 20:14:46 +0000

    update the url Database with a new url and tiny url via the POST method

--- express_server.js
+++ express_server.js
@@ -52,15 +52,24 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
-});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+
+app.post("/urls", (req, res) => {
+  // console.log(generateRandomString(), req.body);
+  const tiny = generateRandomString()
+  console.log(urlDatabase);
+  // const newUrl = {longURL: req.body.longURL}
+  urlDatabase[tiny] = req.body.longURL
+  // urlDatabase[tiny] = newUrl
+  console.log(urlDatabase);
+  res.send("Ok");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
