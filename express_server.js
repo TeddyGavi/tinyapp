40bcfde0d41529b0d8f719342ecdfbd81f2f5d62
Matthew Davis <matc.davis@gmail.com>
2022-08-18 20:03:50 +0000

    add code register ejs page

--- express_server.js
+++ express_server.js
@@ -99,6 +99,14 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 })
 
+//register page
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies.username,
+  }
+  res.render("urls_register", templateVars)
+})
+
 /**************************POST********************************/
 
 //form submission handling by updating the database
